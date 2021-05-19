7c65ce9e1c3d54b99f35f45983f8093e873883a9
moniquetrotta <monique.trotta@gmail.com>
2021-05-19 20:55:40 -0300

    versao 1.0

---
+++ js/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () =>{
+let tl = new TimelineMax();
+tl.fromTo('.bg-loader', 1,
+{width: '100%'},
+{width: '0%', delay:6 , ease: Expo.easeInOut})
+
+tl.fromTo('.bg-video', 2,
+{width: '0%', opacity: 0},
+{width: '100%', opacity: 1, ease: Expo.easeInOut}, '-=1')
+
+tl.fromTo('.logo', 0.9,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.nav-list', 0.9,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.nav-social', 0.9,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.item-1', 1,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.item-2', 1,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.item-3', 1,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+tl.fromTo('.item-4', 1,
+{y: -50, opacity: 0},
+{y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+})
